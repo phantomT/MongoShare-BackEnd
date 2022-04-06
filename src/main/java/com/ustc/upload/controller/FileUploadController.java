a1fd690fdfc522da85ec2bd98f2beb50e285892d
phantomT <bobphantom@126.com>
2022-04-07 01:10:17 +0800

    "Add gitignore"

--- src/main/java/com/ustc/upload/controller/FileUploadController.java
+++ src/main/java/com/ustc/upload/controller/FileUploadController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
